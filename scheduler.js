66b2f9c234ce7cd8387e0ced2cba1c33d2093aa6
Darren King <darrenbrettking@hotmail.com>
2019-09-14 23:25:39 -0400

    Cleaned up scheduler

--- scheduler.js
+++ scheduler.js
@@ -36,17 +36,3 @@ for (let team of teamPool) {
   let b = schedule.filter(e => e.includes(team));
   console.log(`The ${team} have ${b.length} games`);
 }
-
-
-let creightonGames = schedule.slice(0, 32);
-
-// console.log('creightonGames: ', creightonGames);
-
-console.log('schedule.length: ', schedule.length);
-let firstHalfSchedule = schedule.slice(0, 96);
-console.log('firstHalfSchedule: ', firstHalfSchedule);
-
-let secondHalfSchedule = schedule.slice(96, 192);
-console.log('secondHalfSchedule: ', secondHalfSchedule);
-
-console.log('totalGames: ', firstHalfSchedule.length + secondHalfSchedule.length);
